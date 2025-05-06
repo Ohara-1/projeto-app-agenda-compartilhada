3aa2e9399ea03449973f2f8fc62a5e581abfbff2
Ohara-1 <rafael.ohara@hotmail.com>
2025-05-06 13:16:51 -0300

    Add files via upload

--- projeto-app-agenda-compartilhada-1.0-V/backend/run.py
+++ projeto-app-agenda-compartilhada-1.0-V/backend/run.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from pathlib import Path
-
+#sla
 # Adiciona o diretório raiz do projeto ao PYTHONPATH
 ROOT_DIR = Path(__file__).parent.parent
 sys.path.insert(0, str(ROOT_DIR))
